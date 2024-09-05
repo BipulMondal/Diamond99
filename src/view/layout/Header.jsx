8686961bcc08ace4be9330c00af9a1aa63802068
Bipul Mondal <bipulmondal3007@gmail.com>
2024-09-05 06:57:31 +0530

    changes

--- src/view/layout/Header.jsx
+++ src/view/layout/Header.jsx
@@ -5,31 +5,37 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <>
-    <div className="header_main">
-      <div className="header_left">
-        <div>
-          <img
-            className="header_logo"
-            src={logo}
-            alt="logo"
-            onClick={() => {
-              navigate("/");
-            }}
-          />
+      <div className="header_main">
+        <div className="header_left">
+          <div>
+            <img
+              className="header_logo"
+              src={logo}
+              alt="logo"
+              onClick={() => {
+                navigate("/");
+              }}
+            />
+          </div>
         </div>
-      </div>
-      <div className="herder_right">
-        <div><i class="fa fa-search-plus" aria-hidden="true"></i></div>
-        <div>Rules</div>
-        <div>Blance:1500</div>
-        <div><select class="form-control" id="exampleFormControlSelect1">
-                  <option selected>Demo</option>
-                  <option value="1">One</option>
-                  <option value="2">Two</option>
-                  <option value="3">Three</option>
-                </select>
-                </div>
-      </div>
+        <div className="herder_right">
+          <div className="header_search_icon">
+            <i class="fa fa-search-plus" aria-hidden="true"></i>
+          </div>
+          <div className="header_text1">Rules</div>
+          <div className="header_text2">
+            <h6>Blance:1500</h6>
+            <h6>Exp:0</h6>
+          </div>
+          <div>
+            <select class="form-control" id="exampleFormControlSelect1">
+              <option selected>Demo</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
       </div>
       <div className="header_list">
         <ul>
@@ -40,10 +46,7 @@ const Header = () => {
           <li>TENNIS</li>
         </ul>
       </div>
-      </>
-      
-      
-      
+    </>
   );
 };
 
